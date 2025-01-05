67b19cfdf0ddff19a008a1b2707268eb1e563e0b
K. <121282824+JdotOrg@users.noreply.github.com>
2025-01-06 01:16:16 +0530

    Update admin.py

--- telegram_bot/telegram_event_booking/events/admin.py
+++ telegram_bot/telegram_event_booking/events/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from events.models import Event, Ticket
 
-# Register your models here.
-from django.contrib import admin
-from .models import Event
+# Display booking history in Ticket Admin
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "ticket_id", "event", "created_at")  # Fields to display
+    search_fields = ("user_name", "ticket_id", "event__name")         # Add search functionality
+    list_filter = ("event", "created_at")                            # Add filtering options
 
+# Register models in admin
 admin.site.register(Event)
+admin.site.register(Ticket, TicketAdmin)
