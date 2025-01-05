eb5d8310f451dadf35bc720afb2cd517d83e82a8
K. <121282824+JdotOrg@users.noreply.github.com>
2025-01-06 00:00:18 +0530

    Update models.py
    Added ticket function to generate ticket id and bar code

--- telegram_bot/telegram_event_booking/events/models.py
+++ telegram_bot/telegram_event_booking/events/models.py
@@ -1,6 +1,8 @@
 # Create your models here.
 from django.db import models
+import uuid
 
+# Events models to display events details on webserver and on telegram bot
 class Event(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -10,3 +12,14 @@ class Event(models.Model):
     
     def __str__(self):
         return self.name
+    
+# Tickets models to store ticket details and barcode images -- Telegram Bot
+class Ticket(models.Model):
+    user_id = models.CharField(max_length=255)  # Telegram user ID
+    user_name = models.CharField(max_length=255)  # User's name
+    event = models.ForeignKey('Event', on_delete=models.CASCADE)
+    ticket_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)  # Random unique ticket ID
+    barcode_image = models.ImageField(upload_to='barcodes/', blank=True, null=True)  # Path to barcode image
+
+    def __str__(self):
+        return f"{self.user_name} - {self.event.name}"
