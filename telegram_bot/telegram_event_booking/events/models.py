e4f94f7d846eb1472022ac8e2f46c2151be4cdc7
K. <121282824+JdotOrg@users.noreply.github.com>
2025-01-27 16:58:36 +0530

    Update models.py
    added thumbnail and barcode

--- telegram_bot/telegram_event_booking/events/models.py
+++ telegram_bot/telegram_event_booking/events/models.py
@@ -9,9 +9,19 @@ class Event(models.Model):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     location = models.CharField(max_length=255)
+    thumbnail = models.ImageField(upload_to='event_thumbnails/', null=True, blank=True)  # New field for thumbnail
     
     def __str__(self):
         return self.name
+    
+     # Display thumbnail in the admin panel
+    def thumbnail_tag(self):
+        if self.thumbnail:
+            return f'<img src="{self.thumbnail.url}" style="width: 50px; height: 50px; object-fit: cover; border-radius: 5px;" />'
+        return "No Image"
+
+    thumbnail_tag.short_description = "Thumbnail"
+    thumbnail_tag.allow_tags = True
     
 # Tickets models to store ticket details and barcode images -- Telegram Bot
 class Ticket(models.Model):
@@ -20,6 +30,7 @@ class Ticket(models.Model):
     event = models.ForeignKey('Event', on_delete=models.CASCADE)
     ticket_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)  # Random unique ticket ID
     barcode_image = models.ImageField(upload_to='barcodes/', blank=True, null=True)  # Path to barcode image
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user_name} - {self.event.name}"
